9942bc30435925ee0bcab4d34ad829dbf13437a1
JasurbekZiyodullayev <ziyodullayevjasurbek123@gmail.com>
2024-07-09 14:28:25 +0500

    table component add

---
+++ src/components/table/index.tsx
@@ -0,0 +1,66 @@
+import { Flex, Loader, Table } from "@mantine/core";
+import { useEffect, useRef } from "react";
+
+export default function TableComponent({
+  rows,
+  isFetching,
+  thead,
+  setExcel,
+  show,
+  maxWidth,
+}: any) {
+  const tableRef = useRef(null);
+  useEffect(() => {
+    show && setExcel(tableRef.current);
+  }, [tableRef]);
+
+  return (
+    <div className="scroll-element">
+      <div
+        className="table-inner"
+        style={{ maxWidth: maxWidth ? "calc(100vw - 350px)" : "" }}
+      >
+        <Table
+          ref={tableRef}
+          withColumnBorders
+          borderColor="transparent"
+          // borderColor={"var(--tb-color)"}
+          verticalSpacing="sm"
+          horizontalSpacing="sm"
+          styles={{
+            table: {
+              borderRadius: "0.5rem",
+            },
+            thead: {
+              backgroundColor: "var(--primary-color)",
+              color: "var(--white)",
+            },
+            th: {
+              fontSize: "0.875rem",
+              fontWeight: "600",
+            },
+            tbody: { backgroundColor: "var(--paper-bg)" },
+            td: {
+              color: "var(--grey-600)",
+              fontSize: "0.875rem",
+              fontWeight: 500,
+              borderBottom: "1px solid var(--tb-color)",
+            },
+          }}
+        >
+          <Table.Thead>{thead}</Table.Thead>
+          <Table.Tbody>{!isFetching && rows}</Table.Tbody>
+
+          {isFetching && (
+            <Table.Caption>
+              <Flex align={"center"} justify={"center"} gap={16} p={16}>
+                Yuklanmoqda
+                <Loader size={16} color="var(--grey-600)" type="dots" />
+              </Flex>
+            </Table.Caption>
+          )}
+        </Table>
+      </div>
+    </div>
+  );
+}
