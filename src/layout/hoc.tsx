27098bf578d53b0ce1cc6d77f1526e2ad79bd1ed
JasurbekZiyodullayev <ziyodullayevjasurbek123@gmail.com>
2024-12-23 14:46:48 +0500

    add hoc

--- src/layout/hoc.tsx
+++ src/layout/hoc.tsx
@@ -1,5 +1,10 @@
+import { TOKEN } from "@/constants/config.api";
 import { ILayout } from "@/types/layout";
+import { Navigate } from "react-router-dom";
 
 export const HocPrivate = ({ children }: ILayout) => {
+  if (!localStorage.getItem(TOKEN)) {
+    return <Navigate to="/" />;
+  }
   return <>{children}</>;
 };
